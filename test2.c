62758d6eea098c5e3d583889b350ea5fc0444e05
oovaa <omarvx211@gmail.com>
2023-08-02 05:17:25 +0200

    test2 modidied

--- test2.c
+++ test2.c
@@ -1,20 +1,38 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[]){
-const double PI= 3.14159;
-double r;
-double c;
-printf("enter radios\n ");
+int main(int argc, char const *argv[]) {
+  const double PI = 3.14159;
+  double r;
+  double c;
+  char s1[22];
+  char s2[22];
+//   printf("enter name\n");
+//   scanf("%s", &s1);
 
-scanf("%lf",&r);
+//   printf("enter name\n");
+//   scanf("%s", &s2);
 
-double re = 2*PI*r;
-double area = PI * r*r;
+//   char s1[22];
+//   char s2[22];
 
+  printf("enter name\n");
+  scanf("%s", s1);
 
-printf("the circ is %lf \n ",re);
-printf("the area is %lf ",area);
+  printf("enter name\n");
+  scanf("%s", s2);
 
-    return 0;
+  printf("\n%s and %s\n", s1, s2);
+
+  // printf("enter radios\n ");
+
+  // scanf("%lf",&r);
+
+  // double re = 2*PI*r;
+  // double area = PI * r*r;
+
+  // printf("the circ is %lf \n ",re);
+  // printf("the area is %lf ",area);
+
+  return 0;
 }
