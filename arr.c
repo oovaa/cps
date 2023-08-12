819f036a2012b6e586bd34e982d5d5b12dddbdf4
oovaa <omarvx211@gmail.com>
2023-08-12 19:18:36 +0200

    c array is weird at 12-19

--- arr.c
+++ arr.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 
 int main() {
-  int nums[][3] = {{3, 6}, {1, 3}, {78, 12}};
 
-  for (int i = 0; i < sizeof(nums) / sizeof(nums[1]); i++) {
-    for (int l = 0; l < sizeof(nums[1]) / sizeof(nums[1][1]); l++) {
-      int x = l + i * sizeof(nums) / sizeof(nums[1]);
-      printf("%d   ", x);
-    }
-  }
+  int a[5] = {2,4,6,7,NULL};
+  // int c = {2,4,6,7,NULL};
+  int i = 0;
+ while (a[i] != NULL ) //hmmmm why ? 🤨
+ {
+  printf("%d\n",a[i++]);
+ }
+ 
+  
+  // int nums[][3] = {{3, 6}, {1, 3}, {78, 12}};
 
-  char s;
-  printf("\nenter a symble: \n");
-  scanf(" %c", &s);
-  printf("enter a symble: \n");
-  scanf(" %c", &s); // the space matters it clears the buffer i think
+  // for (int i = 0; i < sizeof(nums) / sizeof(nums[1]); i++) {
+  //   for (int l = 0; l < sizeof(nums[1]) / sizeof(nums[1][1]); l++) {
+  //     int x = l + i * sizeof(nums) / sizeof(nums[1]);
+  //     printf("%d   ", x);
+  //   }
+  // }
+
+  // char* s;
+  // printf("\nenter a symble: \n");
+  // scanf("%s", &s);
+  // printf("enter a symble: \n");
+  // scanf("%s", s); // the space matters it clears the buffer i think
   // scanf("%c");
-  for (int i = 0; i < 10; i++) {
-    for (int l = i; l < 10; l++) {
-      printf("%c  ", s);
-    }
-    printf("\n");
-  }
+  // for (int i = 0; i < 10; i++) {
+  //   for (int l = i; l < 10; l++) {
+  //     printf("%s",&s);
+  //   }
+  //   printf("\n");
+  // }
 }
