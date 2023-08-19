af15c87103e537cde8b7e657b51e933108813994
oovaa <omarvx211@gmail.com>
2023-08-19 15:33:37 +0200

    C Strings at 19-15

--- str.c
+++ str.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    char s[] = "I am ALX\0 student ";
+    char s[6] ;
+    scanf("%s",s);
+    s[0] = toupper(s[0]);
+    strcat(s,s);
     printf("%ld\n",strlen(s));
     for (size_t i = 0; i < strlen(s); i++)
-    {
-        printf("%c\t",s[i]);
-        
-    }
+        printf("%c",s[i]);
+
+        int t = strcmp(s,s); // 1 if not equal 0  if equal
+        printf("\n%d\n",t);     
+
+    for (size_t i = 0; i < strlen(s); i++)
+    printf("%d\t",isupper(s[i]));
     
-     
     return 0;
 }
 
