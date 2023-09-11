34c921ae239af1e768d28e60632ba9420986e079
oovaa <omarvx211@gmail.com>
2023-09-11 20:21:54 +0200

    practice function pointers at 11-20

--- functionpointers.c
+++ functionpointers.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem_hex(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
+int is_0(int elem);
+
 static void swap(int *a, int *b) {
   int temp = *a;
   *a = *b;
@@ -36,19 +44,60 @@ void exe(void (*fn)()) { fn(); }
 int main() {
   int one[] = {12, 0, 5, 154, 87, 78, 124};
   int size = sizeof(one) / sizeof(int);
+  // array_iterator(one, 7, &print_elem_hex);
+  int index = int_index(one, 7, &is_0);
+  printf("%d\n", index);
 
   // bubble_sort(one, size, is_smaller);
   // bubble_sort(one, size, is_greater);
-  bubble_sort(one, size, &second_digit);
-  print_arr(one, size);
-  printf("%s\n", __DATE__);
-  printf("%s\n", __TIME__);
-  printf("%d\n", __LINE__);
-  printf("%d\n", __STDC__);
+  // bubble_sort(one, size, &second_digit);
+  // print_arr(one, size);
+  // printf("%s\n", __DATE__);
+  // printf("%s\n", __TIME__);
+  // printf("%d\n", __LINE__);
+  // printf("%d\n", __STDC__);
 
-  exe(&sat);
-  exe(&tas);
+  // exe(&sat);
+  // exe(&tas);
+
+  // print_name("omar", &print_name_as_is);
+  // print_name("omar", &print_name_uppercase);
 
   return 0;
-  return 0;
+}
+int int_index(int *array, int size, int (*cmp)(int)) {
+  for (int i = 0; i < size; i++) {
+    if (cmp(array[i]))
+      return i;
+  }
+  return -1;
+}
+
+int is_0(int elem) { return (elem == 0); }
+
+void print_elem_hex(int elem) { printf("%d\n", elem); }
+
+void array_iterator(int *array, size_t size, void (*action)(int)) {
+  for (size_t i = 0; i < size; i++)
+    action(array[i]);
+}
+
+void print_name(char *name, void (*f)(char *)) { f(name); }
+
+void print_name_as_is(char *name) { printf("Hello, my name is %s\n", name); }
+
+void print_name_uppercase(char *name) {
+  unsigned int i;
+
+  printf("Hello, my uppercase name is ");
+  i = 0;
+  while (name[i]) {
+    if (name[i] >= 'a' && name[i] <= 'z') {
+      putchar(name[i] + 'A' - 'a');
+    } else {
+      putchar(name[i]);
+    }
+    i++;
+  }
+  putchar('\n');
 }
